f328fcce35834ff43e79c653e0cb6cd026836bbd
Jakub Bentkowski <jakub.bentkowski@outlook.com>
2016-03-31 16:54:51 +0200

    Basic logger

--- Source/Core/Source/Logger.h
+++ Source/Core/Source/Logger.h
@@ -5,25 +5,30 @@
 #ifndef GAME_LOGGER_H
 #define GAME_LOGGER_H
 
+#include <spdlog/spdlog.h>
+
 namespace Core
 {
-    class Logger
-    {
-    public:
-        static Logger& getInstance()
-        {
-            static Logger instance;
-            return instance;
-        }
-    private:
-        Logger() {};
+	class Logger
+	{
+	public:
+		static Logger& getInstance()
+		{
+			static Logger instance;
+			return instance;
+		}
 
-    public:
-        Logger(Logger const&) = delete;
-        Logger(Logger&&) = delete;
-        void operator=(Logger const&) = delete;
-        void operator=(Logger&&) = delete;
-    };
+		std::shared_ptr<spdlog::logger> Console;
+
+	private:
+		Logger() : Console(spdlog::stdout_logger_mt("console")) { };
+
+	public:
+		Logger(Logger const&) = delete;
+		Logger(Logger&&) = delete;
+		void operator=(Logger const&) = delete;
+		void operator=(Logger&&) = delete;
+	};
 }
 
 #endif //GAME_LOGGER_H
