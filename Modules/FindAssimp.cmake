4dbc311928e5adda3ee2ea91dffc53ef39810911
MrJaqbq <bentkowski.jakub@gmail.com>
2017-09-17 13:31:21 +0200

    Dependencies build from source if not found extended yage_setup_dependency function

--- Modules/FindAssimp.cmake
+++ Modules/FindAssimp.cmake
@@ -1,81 +1,29 @@
-if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set(ASSIMP_ARCHITECTURE "64")
-elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
-    set(ASSIMP_ARCHITECTURE "32")
-endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
+# Find Assimp
+# Find the native Assimp includes and libraries
+#
+# ASSIMP_INCLUDE_DIR - where to find headers
+# ASSIMP_LIBRARIES   - List of libraries when using libAssimp.
+# ASSIMP_FOUND       - True if libAssimp found.
 
-if(WIN32)
-    set(ASSIMP_ROOT_DIR CACHE PATH "ASSIMP root directory")
+if(ASSIMP_INCLUDE_DIR)
+    # Already in cache, be silent
+    set(ASSIMP_FIND_QUIETLY TRUE)
+endif(ASSIMP_INCLUDE_DIR)
 
-    # Find path of each library
-    find_path(ASSIMP_INCLUDE_DIR
-            NAMES
-            assimp/anim.h
-            HINTS
-            ${ASSIMP_ROOT_DIR}/include
-            )
+find_path(ASSIMP_INCLUDE_DIR
+        NAMES assimp/postprocess.h assimp/scene.h assimp/version.h assimp/config.h assimp/cimport.h
+        PATHS /usr/include/ /usr/local/include/)
+find_library(ASSIMP_LIBRARY NAMES libassimp.so.4.0.1 libassimp.so.4 libassimp.so assimp)
 
-    if(MSVC12)
-        set(ASSIMP_MSVC_VERSION "vc120")
-    elseif(MSVC14)
-        set(ASSIMP_MSVC_VERSION "vc140")
-    endif(MSVC12)
+# Handle the QUIETLY and REQUIRED arguments and set GLFW_FOUND to TRUE if
+# all listed variables are TRUE.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ASSIMP DEFAULT_MSG ASSIMP_LIBRARY ASSIMP_INCLUDE_DIR)
 
-    if(MSVC12 OR MSVC14)
+if(ASSIMP_FOUND)
+    set(ASSIMP_LIBRARIES ${ASSIMP_LIBRARY})
+else()
+    set(ASSIMP_LIBRARIES)
+endif()
 
-        find_path(ASSIMP_LIBRARY_DIR
-                NAMES
-                assimp-${ASSIMP_MSVC_VERSION}-mt.lib
-                HINTS
-                ${ASSIMP_ROOT_DIR}/lib${ASSIMP_ARCHITECTURE}
-                )
-
-        find_library(ASSIMP_LIBRARY_RELEASE				assimp-${ASSIMP_MSVC_VERSION}-mt.lib 			PATHS ${ASSIMP_LIBRARY_DIR})
-        find_library(ASSIMP_LIBRARY_DEBUG				assimp-${ASSIMP_MSVC_VERSION}-mtd.lib			PATHS ${ASSIMP_LIBRARY_DIR})
-
-        set(ASSIMP_LIBRARY
-                optimized 	${ASSIMP_LIBRARY_RELEASE}
-                debug		${ASSIMP_LIBRARY_DEBUG}
-                )
-
-        set(ASSIMP_LIBRARIES "ASSIMP_LIBRARY_RELEASE" "ASSIMP_LIBRARY_DEBUG")
-
-        FUNCTION(ASSIMP_COPY_BINARIES TargetDirectory)
-            ADD_CUSTOM_TARGET(AssimpCopyBinaries
-                    COMMAND ${CMAKE_COMMAND} -E copy ${ASSIMP_ROOT_DIR}/bin${ASSIMP_ARCHITECTURE}/assimp-${ASSIMP_MSVC_VERSION}-mtd.dll 	${TargetDirectory}/Debug/assimp-${ASSIMP_MSVC_VERSION}-mtd.dll
-                    COMMAND ${CMAKE_COMMAND} -E copy ${ASSIMP_ROOT_DIR}/bin${ASSIMP_ARCHITECTURE}/assimp-${ASSIMP_MSVC_VERSION}-mt.dll 		${TargetDirectory}/Release/assimp-${ASSIMP_MSVC_VERSION}-mt.dll
-                    COMMENT "Copying Assimp binaries to '${TargetDirectory}'"
-                    VERBATIM)
-        ENDFUNCTION(ASSIMP_COPY_BINARIES)
-
-    endif()
-
-else(WIN32)
-
-    find_path(
-            assimp_INCLUDE_DIRS
-            NAMES postprocess.h scene.h version.h config.h cimport.h
-            PATHS /usr/local/include/
-    )
-
-    find_library(
-            assimp_LIBRARIES
-            NAMES assimp
-            PATHS /usr/local/lib/
-    )
-
-    if (assimp_INCLUDE_DIRS AND assimp_LIBRARIES)
-        SET(assimp_FOUND TRUE)
-    ENDIF (assimp_INCLUDE_DIRS AND assimp_LIBRARIES)
-
-    if (assimp_FOUND)
-        if (NOT assimp_FIND_QUIETLY)
-            message(STATUS "Found asset importer library: ${assimp_LIBRARIES}")
-        endif (NOT assimp_FIND_QUIETLY)
-    else (assimp_FOUND)
-        if (assimp_FIND_REQUIRED)
-            message(FATAL_ERROR "Could not find asset importer library")
-        endif (assimp_FIND_REQUIRED)
-    endif (assimp_FOUND)
-
-endif(WIN32)
+mark_as_advanced(ASSIMP_INCLUDE_DIR ASSIMP_LIBRARY)
--- Modules/YageSetupDependency.cmake
+++ Modules/YageSetupDependency.cmake
@@ -1,42 +1,58 @@
 # Setup Dependency
 #
 # Provides procedure to easily setup dependency that can both be build from source or get from environment
+function(yage_setup_dependency NAME)
+    set(options TEST)
+    set(oneArg PREFER TARGET SOURCE VAR_NAME)
+    set(multiArg INCLUDE)
+    cmake_parse_arguments(DEP "${options}" "${oneArg}" "${multiArg}" ${ARGN} )
 
-function(yage_setup_dependency NAME MODE) # Source Directory, Include Directory, Target name
-    if(MODE STREQUAL "SOURCE")
-        # provide option to build from source or get from os
-        option(YAGE_USE_BUNDLED_${NAME} "Build ${NAME} from source" ON)
+    if (NOT DEP_TARGET)
+        set (DEP_TARGET ${NAME})
     endif()
 
-    if((NOT YAGE_USE_BUNDLED_${NAME}) OR (MODE STREQUAL "OS"))
-        # use find_package to get this target from OS
-        message("-- Get " ${NAME} " from OS")
-        find_package(${NAME})
+    if (NOT DEP_VAR_NAME)
+        set (DEP_VAR_NAME ${NAME})
+    endif()
 
-    elseif(YAGE_USE_BUNDLED_${NAME})
-            # build this target from bundled source
-            message("-- Build " ${NAME} " from source")
+    if (NOT NAME)
+        message(FATAL_ERROR "yage_setup_dependency called without specyfing name!\n")
+    else()
+        if (NOT DEP_PREFER STREQUAL "SOURCE")
+            find_package(${NAME})
+            message("-- Search for " ${NAME} " in OS")
+            if (${DEP_VAR_NAME}_FOUND)
+                message("--   " ${NAME} " found!")
+            else()
+                message("--   " ${NAME} " not found!")
+            endif()
+        endif()
 
-            if(ARGC GREATER 4)
-
-                message("--   " ${NAME} " source dir " ${ARGV2}
-                        "\n--   " ${NAME} " include dir " ${ARGV3}
-                        "\n--   " ${NAME} " target name " ${ARGV4})
-
-                add_subdirectory(${ARGV2} EXCLUDE_FROM_ALL)
-                set(${NAME}_TARGET ${ARGV4} PARENT_SCOPE)
-                set(${NAME}_INCLUDE_DIR ${ARGV3} PARENT_SCOPE)
-
-                set(${NAME}_LIBRARY ${ARGV4} PARENT_SCOPE)
-                set(${NAME}_LIBRARIES ${ARGV4} PARENT_SCOPE)
-
-            # source dir is specified
-            elseif(ARGC GREATER 2)
-                message("--   " ${NAME} " source dir " ${ARGV2})
-                add_subdirectory(${ARGV2} EXCLUDE_FROM_ALL)
+        if ((DEP_PREFER STREQUAL "BUILD") OR (NOT ${DEP_VAR_NAME}_FOUND))
+            if (DEP_TARGET STREQUAL NAME)
+                message ("-- building " ${NAME} " from source!")
+            else()
+                message ("-- building " ${NAME} " from source as target " ${DEP_TARGET} "!")
             endif()
 
-    elseif(NOT (MODE STREQUAL "BUNDLE"))
-        message(FATAL_ERROR "yage_setup_dependency called with unknown argument(s):\n  " ${MODE})
+            if (DEP_INCLUDE)
+                foreach (incl IN ITEMS ${DEP_INCLUDE})
+                    message("--   " ${NAME} " include dir " ${incl})
+                endforeach()
+            endif()
+
+            if (DEP_SOURCE)
+                message("--   " ${NAME} " source dir " ${DEP_SOURCE})
+
+                add_subdirectory(${DEP_SOURCE} EXCLUDE_FROM_ALL)
+            endif()
+
+            set(${DEP_VAR_NAME}_TARGET ${DEP_TARGET} PARENT_SCOPE)
+            set(${DEP_VAR_NAME}_INCLUDE_DIR ${DEP_INCLUDE} PARENT_SCOPE)
+
+            set(${DEP_VAR_NAME}_LIBRARY ${DEP_TARGET} PARENT_SCOPE)
+            set(${DEP_VAR_NAME}_LIBRARIES ${DEP_TARGET} PARENT_SCOPE)
+
+        endif()
     endif()
 endfunction()
