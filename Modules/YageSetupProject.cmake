00ed8d62d4bae79c44ed40330c473e94f85c86fb
BentouDev <bentkowski.jakub@gmail.com>
2018-08-02 14:19:30 +0200

    Extracted cmake project setup to separate file

---
+++ Modules/YageSetupProject.cmake
@@ -0,0 +1,47 @@
+#============================#
+# Project setup              #
+#============================#
+# detect if YAGE is being bundled,
+# disable testsuite in that case
+if(NOT DEFINED PROJECT_NAME)
+  set(NOT_SUBPROJECT ON)
+endif()
+
+# Add modules
+set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Modules" ${CMAKE_MODULE_PATH})
+include (Conan)
+include (YageSetupDependency)
+
+# Configure conan
+set (YAGE_CONAN_INFO ${CMAKE_CURRENT_SOURCE_DIR}/conanbuildinfo.cmake)
+if (NOT EXISTS YAGE_CONAN_INFO)
+    set(YAGE_CONAN_INFO ${CMAKE_CURRENT_SOURCE_DIR}/Build/conanbuildinfo.cmake)
+endif()
+
+if(EXISTS YAGE_CONAN_INFO)
+    include (YAGE_CONAN_INFO)
+    conan_basic_setup()
+endif()
+
+# Emit additional metadata for debug builds
+include(CMakeToolsHelpers OPTIONAL)
+
+# Pick either lib or lib64
+include("GNUInstallDirs")
+
+# Installation
+if(NOT LIBDIR)
+    set (LIBDIR lib)
+endif()
+
+set (YAGE_BINARY_DIR "bin")
+set (YAGE_LIBRARY_DIR "${LIBDIR}/YAGE")
+set (YAGE_INCLUDE_DIR "include/YAGE")
+
+# Enforce warnings
+if(CMAKE_COMPILER_IS_GNUCC)
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Wall")
+endif(CMAKE_COMPILER_IS_GNUCC)
+if(MSVC)
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} /W4")
+endif(MSVC)
