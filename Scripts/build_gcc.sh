ceb72ba6dc9dea12b1a8a40a5d9137a870a6d067
BentouDev <bentkowski.jakub@gmail.com>
2018-08-03 12:22:05 +0200

    CI: Attempt to force test build on travis with gcc and appveyor

--- Scripts/build_gcc.sh
+++ Scripts/build_gcc.sh
@@ -8,5 +8,5 @@ mkdir Build
 cd Build
 conan remote add yage https://api.bintray.com/conan/bentoudev/yage
 conan install ..
-cmake --target all -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage " -DENABLE_COVERAGE=On -DCMAKE_BUILD_TYPE=Release -DYAGE_VERSION_PATCH=$PATCH_VER ..
+cmake --target all -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage " -DYAGE_BUILD_TESTS=On -DENABLE_COVERAGE=On -DCMAKE_BUILD_TYPE=Release -DYAGE_VERSION_PATCH=$PATCH_VER ..
 make
