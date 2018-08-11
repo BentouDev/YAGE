bbff200589cfeb2acc1040199f92ce480001dcd5
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-08-12 01:00:40 +0200

    Revert "Remove conan install step from build - conan is only used by CMake"
    This reverts commit c8b5dd611545fa3f6add48015deddccd8d9866f7.

--- Scripts/build_clang.sh
+++ Scripts/build_clang.sh
@@ -7,5 +7,6 @@ conan --version
 mkdir Build
 cd Build
 conan remote add yage https://api.bintray.com/conan/bentoudev/yage
+conan install ..
 cmake --target all -DCMAKE_BUILD_TYPE=Release -DYAGE_VERSION_PATCH=$PATCH_VER ..
 make
--- Scripts/build_gcc.sh
+++ Scripts/build_gcc.sh
@@ -7,5 +7,6 @@ conan --version
 mkdir Build
 cd Build
 conan remote add yage https://api.bintray.com/conan/bentoudev/yage
+conan install ..
 cmake --target all -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage " -DYAGE_BUILD_TESTS=On -DENABLE_COVERAGE=On -DCMAKE_BUILD_TYPE=Release -DYAGE_VERSION_PATCH=$PATCH_VER ..
 make
