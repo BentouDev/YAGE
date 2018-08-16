8f5cf95149c8bf7860d1a9c78f5c16aa7397fa5e
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-08-16 23:20:11 +0200

    Some libs just have to be built from source...

--- Dependencies/Spdlog/Spdlog.cmake
+++ Dependencies/Spdlog/Spdlog.cmake
@@ -1,7 +1,8 @@
 #Spdlog
 yage_setup_dependency(Spdlog
         NOT_OS TRUE
-        CONAN spdlog/1.0.0@bincrafters/stable
-        VAR_NAME SPDLOG)
+        VAR_NAME SPDLOG
+        SOURCE Dependencies/Spdlog/Source
+        INCLUDE Dependencies/Spdlog/Source/include)
 
 yage_include(YAGE ${SPDLOG_INCLUDE_DIR})
