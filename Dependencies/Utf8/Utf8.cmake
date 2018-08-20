a038dfde9260a514798c394f2cf3f8b06b30f3a3
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-08-20 22:25:30 +0200

    Fixed invalid utf8 dependency setup

--- Dependencies/Utf8/Utf8.cmake
+++ Dependencies/Utf8/Utf8.cmake
@@ -1,3 +1,11 @@
 # Utf8
 add_library(utf8 INTERFACE)
-target_include_directories(utf8 INTERFACE Dependencies/Utf8/Source/source)
+target_include_directories(utf8
+    INTERFACE 
+        $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/Dependencies/Utf8/Source/source>)
+
+install(TARGETS utf8
+        EXPORT Engine DESTINATION "${YAGE_LIBRARY_DIR}"
+        ARCHIVE  DESTINATION ${YAGE_LIBRARY_DIR}
+        LIBRARY  DESTINATION ${YAGE_LIBRARY_DIR}
+        RUNTIME  DESTINATION ${YAGE_BINARY_DIR})
