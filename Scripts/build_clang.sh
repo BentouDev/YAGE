b9caea667cc6de390f013c62ed63a9e79a25aa53
BentouDev <bentkowski.jakub@gmail.com>
2019-08-21 22:10:46 +0200

    Dont call conan install before cmake does!

--- Scripts/build_clang.sh
+++ Scripts/build_clang.sh
@@ -6,8 +6,8 @@ cmake --version
 conan --version
 mkdir Build
 cd Build
-conan remote add yage https://api.bintray.com/conan/bentoudev/yage
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan install ..
+#conan remote add yage https://api.bintray.com/conan/bentoudev/yage
+#conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+#conan install ..
 cmake --target all -DCMAKE_BUILD_TYPE=Release -DYAGE_VERSION_PATCH=$PATCH_VER ..
 make
