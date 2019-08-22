875cf4ceba0f93258db4d19e322515d10def7e6c
BentouDev <bentkowski.jakub@gmail.com>
2019-08-22 23:43:07 +0200

    Azure: build without specyfing target

--- Scripts/build_azure.bat
+++ Scripts/build_azure.bat
@@ -10,4 +10,4 @@ cd Build
 conan remote add yage https://api.bintray.com/conan/bentoudev/yage
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 cmake --target all -DCMAKE_BUILD_TYPE=Release -DYAGE_VERSION_PATCH=%PATCH_VER% -DYAGE_BUILD_EDITOR=OFF ..
-cmake --build . --target all
+cmake --build .
